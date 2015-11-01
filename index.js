67ad9be030d212a4f2d28dbb6c0d8174d776c2d6
Roy Martin <roy.martin@modop.com>
2015-10-31 23:07:41 -0700

    Add new relic monitoring.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ require('./core/server/utils/startup-check').check();
 express = require('express');
 ghost = require('./core');
 errors = require('./core/server/errors');
+newrelic = require('newrelic');
 
 // Create our parent express app instance.
 parentApp = express();
